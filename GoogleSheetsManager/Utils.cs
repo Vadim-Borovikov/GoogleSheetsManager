12cc95d07ed23705cd73ebf6381a78f69a64a136
Vadim Borovikov <vadim.borovikov@gmail.com>
2022-02-22 14:56:34 +0300

    More universal To* methods

--- GoogleSheetsManager/Utils.cs
+++ GoogleSheetsManager/Utils.cs
@@ -9,10 +9,21 @@ namespace GoogleSheetsManager;
 [PublicAPI]
 public static class Utils
 {
-    public static TimeSpan? ToTimeSpan(this object? o) => ToDateTime(o)?.TimeOfDay;
+    public static TimeSpan? ToTimeSpan(this object? o)
+    {
+        if (o is TimeSpan ts)
+        {
+            return ts;
+        }
+        return ToDateTime(o)?.TimeOfDay;
+    }
 
     public static Uri? ToUri(this object? o)
     {
+        if (o is Uri uri)
+        {
+            return uri;
+        }
         string? uriString = o?.ToString();
         return string.IsNullOrWhiteSpace(uriString) ? null : new Uri(uriString);
     }
@@ -21,31 +32,75 @@ public static class Utils
     {
         return o switch
         {
-            long l => l,
-            double d => (decimal)d,
-            _ => null
+            decimal dec => dec,
+            long l      => l,
+            double d    => (decimal) d,
+            _           => null
         };
     }
 
-    public static int? ToInt(this object? o) => int.TryParse(o?.ToString(), out int i) ? i : null;
+    public static int? ToInt(this object? o)
+    {
+        if (o is int i)
+        {
+            return i;
+        }
+        return int.TryParse(o?.ToString(), out i) ? i : null;
+    }
 
-    public static long? ToLong(this object? o) => long.TryParse(o?.ToString(), out long l) ? l : null;
+    public static long? ToLong(this object? o)
+    {
+        if (o is long l)
+        {
+            return l;
+        }
+        return long.TryParse(o?.ToString(), out l) ? l : null;
+    }
 
-    public static ushort? ToUshort(this object? o) => ushort.TryParse(o?.ToString(), out ushort u) ? u : null;
+    public static ushort? ToUshort(this object? o)
+    {
+        if (o is ushort u)
+        {
+            return u;
+        }
+        return ushort.TryParse(o?.ToString(), out u) ? u : null;
+    }
 
-    public static byte? ToByte(this object? o) => byte.TryParse(o?.ToString(), out byte b) ? b : null;
+    public static byte? ToByte(this object? o)
+    {
+        if (o is byte b)
+        {
+            return b;
+        }
+        return byte.TryParse(o?.ToString(), out b) ? b : null;
+    }
 
-    public static bool? ToBool(this object? o) => bool.TryParse(o?.ToString(), out bool b) ? b : null;
+    public static bool? ToBool(this object? o)
+    {
+        if (o is bool b)
+        {
+            return b;
+        }
+        return bool.TryParse(o?.ToString(), out b) ? b : null;
+    }
 
-    public static List<Uri>? ToUris(this object? o) => o?.ToString()?.Split("\n").Select(ToUri).RemoveNulls().ToList();
+    public static List<Uri>? ToUris(this object? o)
+    {
+        if (o is IEnumerable<Uri> l)
+        {
+            return l.ToList();
+        }
+        return o?.ToString()?.Split("\n").Select(ToUri).RemoveNulls().ToList();
+    }
 
     public static DateTime? ToDateTime(this object? o)
     {
         return o switch
         {
-            double d => DateTime.FromOADate(d),
-            long l   => DateTime.FromOADate(l),
-            _        => null
+            DateTime dt => dt,
+            double d    => DateTime.FromOADate(d),
+            long l      => DateTime.FromOADate(l),
+            _           => null
         };
     }
 }
